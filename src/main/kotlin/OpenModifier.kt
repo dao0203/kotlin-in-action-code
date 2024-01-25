a71da6aa70ec36d671a34112b88637a07cb4a25e
dao0203 <youzaizuoteng872@gmail.com>
2024-01-25 22:22:25 +0900

    try using visibility modifier for property

--- src/main/kotlin/OpenModifier.kt
+++ src/main/kotlin/OpenModifier.kt
@@ -1,12 +1,21 @@
 package org.example
 
 open class BaseClass {
+    //     private val name = "Kotlin"
+//    protected val name = "Kotlin"
+//     internal val name = "Kotlin"
+    val name = "Kotlin"
+
     open fun print() = println("Hello BaseClass")
 }
 
-class SubClass: BaseClass() {
-    override fun print() = println("Hello SubClass")
+class SubClass : BaseClass() {
+
+
+    override fun print() = println("Hello $name")
 }
+
+fun SubClass.getName() = name
 
 fun main() {
     val baseClass = BaseClass()
@@ -14,4 +23,6 @@ fun main() {
 
     val subClass = SubClass()
     subClass.print()
+
+    println(subClass.getName())
 }
